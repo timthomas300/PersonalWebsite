c0e9251bdccd3a0fea55685e7d9a596301eacd61
timthomas300 <timthomas300@gmail.com>
2016-02-12 11:18:05 -0500

    2-11-2016
    Created tabs for portfolio.

---
+++ PersonalWebsite/js/freelancer3.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    if ($('#javascriptTab').hasClass('tabActive') === true) {
+
+    }
+    $(".portfolioTabs").click(function () {
+        $(".portfolioTabs").removeClass('tabActive');
+        $(this).addClass('tabActive');
+        if ($('#javascriptTab').hasClass('tabActive') === true) {
+            $('.portfolioContents').addClass('noDisplay');
+            $('#javascriptTabContent').toggle('fade', 2000);
+            $('#jqueryTabContent').hide();
+            $('#csharpTabContent').hide();
+        }
+        else if ($('#jqueryTab').hasClass('tabActive') === true) {
+            $('.portfolioContents').addClass('noDisplay');
+            $('#jqueryTabContent').toggle('fade', 2000);
+            $('#csharpTabContent').hide();
+            $('#javascriptTabContent').hide();
+        }
+        else if ($('#csharpTab').hasClass('tabActive') === true) {
+            $('.portfolioContents').addClass('noDisplay');
+            $('#csharpTabContent').toggle('fade', 2000);
+            $('#javascriptTabContent').hide();
+            $('#jqueryTabContent').hide();
+        }
+    });
+
+});
